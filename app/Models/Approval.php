b9741c5c7ef1f57d3dbcb30fc58e9a97bb0744af
sanyath007 <sanyath007@gmail.com>
2024-01-16 14:10:11 +0700

    feat: create new Approval and Procuring models

---
+++ app/Models/Approval.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Approval extends Model
+{
+    protected $table = 'approvals';
+    // protected $primaryKey = 'id';
+    // public $incrementing = false; // false = ไม่ใช้ options auto increment
+    // public $timestamps = false; // false = ไม่ใช้ field updated_at และ created_at
+
+    public function requisition()
+    {
+        return $this->belongsTo(Requisition::class, 'requisition_id', 'id');
+    }
+
+    public function procuring()
+    {
+        return $this->belongsTo(Procuring::class, 'procuring_id', 'id');
+    }
+}
