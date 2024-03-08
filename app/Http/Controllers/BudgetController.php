763959cb1175c8dd1a3d3d6cefbb38c6176d4d25
sanyath007 <sanyath007@gmail.com>
2024-03-08 10:23:54 +0700

    reflactor(BudgetController): remove unused method

--- app/Http/Controllers/BudgetController.php
+++ app/Http/Controllers/BudgetController.php
@@ -11,50 +11,6 @@ use App\Models\Budget;
 
 class BudgetController extends Controller
 {
-    public function formValidate (Request $request)
-    {
-        $rules = [
-            'name'          => 'required',
-            'project_id'    => 'required',
-            'unit_id'       => 'required',
-        ];
-
-        if ($request['is_addon'] != '1') {
-            $rules['category_id']       = 'required';
-            $rules['price_per_unit']    = 'required';
-        }
-
-        $messages = [
-            'name.required'             => 'กรุณาระบุชื่อสินค้า/บริการ',
-            'project_id.required'       => 'กรุณาเลือกประเภทแผน',
-            'category_id.required'      => 'กรุณาเลือกประเภทสินค้า/บริการ',
-            'price_per_unit.required'   => 'กรุณาระบุราคาต่อหน่วย',
-            'unit_id.required'          => 'กรุณาเลือกหน่วยนับ',
-        ];
-
-        $validator = \Validator::make($request->all(), $rules, $messages);
-
-        if ($validator->fails()) {
-            $messageBag = $validator->getMessageBag();
-
-            // if (!$messageBag->has('start_date')) {
-            //     if ($this->isDateExistsValidation(convThDateToDbDate($request['start_date']), 'start_date') > 0) {
-            //         $messageBag->add('start_date', 'คุณมีการลาในวันที่ระบุแล้ว');
-            //     }
-            // }
-
-            return [
-                'success' => 0,
-                'errors' => $messageBag->toArray(),
-            ];
-        } else {
-            return [
-                'success' => 1,
-                'errors' => $validator->getMessageBag()->toArray(),
-            ];
-        }
-    }
-
     public function search(Request $req)
     {
         /** Get params from query string */
