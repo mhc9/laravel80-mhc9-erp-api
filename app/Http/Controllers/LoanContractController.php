6a8a9f2d5f6ec1443d3d89f0ac5e75b73ca1625d
sanyath007 <sanyath007@gmail.com>
2024-05-13 11:56:21 +0700

    reflactor(LoanContractController): remove unused approve method and add fields to field list of insertion and updating data of store and update method respectively

--- app/Http/Controllers/LoanContractController.php
+++ app/Http/Controllers/LoanContractController.php
@@ -123,6 +123,11 @@ class LoanContractController extends Controller
         try {
             $contract = new LoanContract();
             $contract->loan_id          = $req['loan_id'];
+            $contract->contract_no      = $req['contract_no'];
+            $contract->approved_date    = $req['approved_date'];
+            $contract->sent_date        = $req['sent_date'];
+            $contract->bill_no          = $req['bill_no'];
+            $contract->bk02_date        = $req['bk02_date'];
             $contract->year             = $req['year'];
             $contract->refund_days      = $req['refund_days'];
             $contract->net_total        = currencyToNumber($req['net_total']);
@@ -166,13 +171,13 @@ class LoanContractController extends Controller
     {
         try {
             $contract = LoanContract::find($id);
-            $contract->contract_no      = $req['contract_no'];
-            $contract->contract_date    = $req['contract_date'];
             $contract->loan_id          = $req['loan_id'];
-            $contract->net_total        = currencyToNumber($req['net_total']);
-            $contract->bill_no          = $req['bill_no'];
+            $contract->contract_no      = $req['contract_no'];
+            $contract->approved_date    = $req['approved_date'];
             $contract->sent_date        = $req['sent_date'];
+            $contract->bill_no          = $req['bill_no'];
             $contract->bk02_date        = $req['bk02_date'];
+            $contract->net_total        = currencyToNumber($req['net_total']);
             $contract->deposit_date     = $req['deposit_date'];
             $contract->refund_days      = $req['refund_days'];
             $contract->remark           = $req['remark'];
@@ -223,57 +228,21 @@ class LoanContractController extends Controller
         }
     }
 
-    public function approve(Request $req, $id)
-    {
-        try {
-            $contract = LoanContract::find($id);
-            $contract->contract_no      = $req['contract_no'];
-            $contract->approved_date    = $req['approved_date'];
-            $contract->sent_date        = $req['sent_date'];
-            $contract->bill_no          = $req['bill_no'];
-            $contract->bk02_date        = $req['bk02_date'];
-            $contract->status           = 2;
-
-            if($contract->save()) {
-                /** อัพเดตตาราง loans โดยเซตค่าฟิลด์ status=3 (3=อนุมัติแล้ว) */
-                Loan::find($contract->loan_id)->update(['status', 3]);
-
-                return [
-                    'status'    => 1,
-                    'message'   => 'Approving successfully!!',
-                    'contract'  => $contract->load('details','details.expense','details.loanDetail','loan.department',
-                                        'loan.employee','loan.employee.prefix','loan.employee.position','loan.employee.level',
-                                        'loan.budgets','loan.budgets.budget','loan.budgets.budget.project','loan.budgets.budget.project.plan',
-                                        'loan.courses','loan.courses.place','loan.courses.place.changwat')
-                ];
-            } else {
-                return [
-                    'status'    => 0,
-                    'message'   => 'Something went wrong!!'
-                ];
-            }
-        } catch (\Exception $ex) {
-            return [
-                'status'    => 0,
-                'message'   => $ex->getMessage()
-            ];
-        }
-    }
-
     public function deposit(Request $req, $id)
     {
         try {
             $contract = LoanContract::find($id);
             $contract->deposited_date   = $req['deposited_date'];
             $contract->refund_date      = $req['refund_date'];
+            $contract->status           = 2;
 
             if($contract->save()) {
                 /** อัพเดตตาราง loans โดยเซตค่าฟิลด์ status=4 (4=เงินเข้าแล้ว) */
-                Loan::find($contract->loan_id)->update(['status', 4]);
+                Loan::find($contract->loan_id)->update(['status' => 4]);
 
                 return [
                     'status'    => 1,
-                    'message'   => 'Approving successfully!!',
+                    'message'   => 'Depositing successfully!!',
                     'contract'  => $contract->load('details','details.expense','details.loanDetail','loan.department',
                                         'loan.employee','loan.employee.prefix','loan.employee.position','loan.employee.level',
                                         'loan.budgets','loan.budgets.budget','loan.budgets.budget.project','loan.budgets.budget.project.plan',
