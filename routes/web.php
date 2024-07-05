1552636b4954c63e0a784db960e5070473249483
sanyath007 <sanyath007@gmail.com>
2024-07-05 11:25:26 +0700

    feat(web.php): add new routes for testing passport API Key Authentication

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +26,36 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/requisitions/{id}/print-pr', 'App\Http\Controllers\RequisitionController@printPR');
 
 Route::get('/cars', [App\Http\Controllers\CarController::class, 'index']);
+
+Route::get('/redirect', function (Request $request) {
+    $request->session()->put('state', $state = Str::random(40));
+
+    $query = http_build_query([
+        'client_id' => '3',
+        'redirect_uri' => 'http://localhost:32772/laravel80-mhc9-erp/public/callback',
+        'response_type' => 'code',
+        'scope' => '',
+        'prompt' => 'none', // "none", "consent", or "login"
+    ]);
+
+    return redirect('http://localhost:5000/oauth/authorize?'.$query);
+});
+
+Route::get('/callback', function (Request $request) {
+    $state = $request->session()->pull('state');
+
+    throw_unless(
+        strlen($state) > 0 && $state === $request->state,
+        InvalidArgumentException::class,
+        'Invalid state value.'
+    );
+
+    $response = Http::asForm()->post('http://localhost:5000/oauth/token', [
+        'grant_type' => 'authorization_code',
+        'client_id' => '3',
+        'redirect_uri' => 'http://localhost:32772/laravel80-mhc9-erp/public/callback',
+        'code' => $request->code,
+    ]);
+
+    return $response->json();
+});
