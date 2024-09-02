f662dd98ad38d64f75fd67eecbce2e1a62df6f3c
sanyath007 <sanyath007@gmail.com>
2024-09-02 10:43:30 +0700

    feat: install new Maatwebsite\Excel package and create new App\Exports\LoanContractExport class that implemented from it

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          */
         Intervention\Image\ImageServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -233,6 +234,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Image' => Intervention\Image\Facades\Image::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
