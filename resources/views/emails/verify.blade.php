a305be5aac109d3eedf25db83c6dff6d0fed3e32
sanyath007 <sanyath007@gmail.com>
2024-09-30 11:44:39 +0700

    reflactor: remove unused buttons in both emails/verify and password views

--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -4,10 +4,6 @@
 Thank you for signing up. 
 Your six-digit code is {{$pin}}
 
-<!-- @component('mail::button', ['url' => ''])
-Button Text
-@endcomponent -->
-
 Thanks,<br>
 {{ config('app.name') }}
 @endcomponent
