a93173cf073cde74d0d6f6d37a9cfa9d8a6a6a04
sanyath007 <sanyath007@gmail.com>
2024-09-30 08:57:15 +0700

    reflactor(ResetPasswordController): implement resetPassword method in ResetPasswordController

--- app/Http/Controllers/ResetPasswordController.php
+++ app/Http/Controllers/ResetPasswordController.php
@@ -10,5 +10,29 @@ use App\Models\User;
 
 class ResetPasswordController extends Controller
 {
-    //
+    public function resetPassword(Request $request)
+    {      
+        $validator = Validator::make($request->all(), [
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+
+        if ($validator->fails()) {
+            return new JsonResponse([
+                'success' => false,
+                'message' => $validator->errors()
+            ], 422);
+        }
+
+        $user = User::where('email', $request->email);
+        $user->update(['password' => Hash::make($request->password)]);
+
+        $token = $user->first()->createToken('myapptoken')->plainTextToken;
+
+        return new JsonResponse([
+            'success'   => true, 
+            'message'   => "Your password has been reset", 
+            'token'     => $token
+        ], 200);
+    }
 }
