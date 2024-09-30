a7e2e8d2c9e17a06a154b319dc1a1cb99a6027c0
sanyath007 <sanyath007@gmail.com>
2024-09-30 08:22:53 +0700

    reflactor: modify handle method of VerifyEmail middleware by checking currentUser's email_verified_at field is null or not and add its link to $routeMiddleware array in Kernel.php

--- app/Http/Middleware/VerifyEmail.php
+++ app/Http/Middleware/VerifyEmail.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 
 class VerifyEmail
 {
@@ -16,6 +18,15 @@ class VerifyEmail
      */
     public function handle(Request $request, Closure $next)
     {
+        if (Auth::user()->email_verified_at === null) {
+            return new JsonResponse(
+                [
+                    'success' => false,
+                    'message' => 'Please verify your email before you can continue'
+                ], 401
+            );
+        }
+
         return $next($request);
     }
 }
