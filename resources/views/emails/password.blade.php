9658d0b0970c644cd2b3a49583b13fb9cb446614
sanyath007 <sanyath007@gmail.com>
2024-10-10 14:18:37 +0700

    reflactor: change email view messages of both emails/password.bladde.php and initial-user.blade.php

--- resources/views/emails/password.blade.php
+++ resources/views/emails/password.blade.php
@@ -1,8 +1,9 @@
 @component('mail::message')
 # Reset Password
 
-Your six-digit PIN is <h4>{{$pin}}</h4>
-<p>Please do not share your One Time Pin With Anyone. You made a request to reset your password. Please discard if this wasn't you.</p>
+รหัส PIN 6 หลัก ของคุณคือ <h4>{{$pin}}</h4>
+<p>กรุณาอย่าเปิดเผย รหัส PIN แก่ผู้อื่นๆ หรือ ผู้ไม่เกี่ยวข้อง</p>
+<p>You made a request to reset your password. Please discard if this wasn't you.</p>
 
 Thanks,<br>
 {{ config('app.name') }}
