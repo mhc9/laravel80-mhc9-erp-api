683d5d1ca9f4c74e0695e9267e0c1f97b53cf472
sanyath007 <sanyath007@gmail.com>
2024-10-29 10:56:40 +0700

    reflactor(BudgetController): add year var and set where clause with condition according to it value in getAll method

--- app/Http/Controllers/BudgetController.php
+++ app/Http/Controllers/BudgetController.php
@@ -61,18 +61,24 @@ class BudgetController extends Controller
         $project    = $req->get('project');
         $plan       = $req->get('plan');
         $name       = $req->get('name');
+        $year       = $req->get('year');
         $status     = $req->get('status');
 
         $activities = Budget::with('activity','type')
-                    // ->when(!empty($project), function($q) use ($project) {
-                    //     $q->where('project_id', $project);
-                    // })
-                    // ->when(!empty($plan), function($q) use ($plan) {
-                    //     $q->whereHas('project.plan', function($sq) use ($plan) {
-                    //         $sq->where('plan_id', $plan);
-                    //     });
-                    // })
-                    ->get();
+                            // ->when(!empty($project), function($q) use ($project) {
+                            //     $q->where('project_id', $project);
+                            // })
+                            // ->when(!empty($plan), function($q) use ($plan) {
+                            //     $q->whereHas('project.plan', function($sq) use ($plan) {
+                            //         $sq->where('plan_id', $plan);
+                            //     });
+                            // })
+                            ->when(!empty($year), function($q) use ($year) {
+                                $q->whereHas('activity', function($sq) use ($year) {
+                                    $sq->where('year', $year);
+                                });
+                            })
+                            ->get();
 
         return $activities;
     }
