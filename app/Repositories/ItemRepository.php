df0a5ad8e1ed7b5996ca1de4b56f1ad51a748742
sanyath007 <sanyath007@gmail.com>
2025-04-02 15:37:31 +0700

    reflactor(ItemRepository): extends BaseRepository and remove duplicated methods

--- app/Repositories/ItemRepository.php
+++ app/Repositories/ItemRepository.php
@@ -2,10 +2,15 @@
 
 namespace App\Repositories;
 
+use App\Repositories\BaseRepository;
+use App\Repositories\Traits\Sortable;
+use App\Repositories\Traits\Relationable;
 use App\Models\Item;
 
-class ItemRepository
+class ItemRepository extends BaseRepository
 {
+    use Sortable, Relationable;
+
     /**
      *  @var $model
      */
@@ -14,36 +19,5 @@ class ItemRepository
     public function __construct(Item $model)
     {
         $this->model = $model;
-    }
-
-    public function getItem($id)
-    {
-        return $this->model->find($id);
-    }
-
-    public function getItems()
-    {
-        return $this->model
-                    ->with('category','unit')
-                    ->get();
-    }
-
-    public function getItemById($id)
-    {
-        return $this->model
-                    ->with('category','unit')
-                    ->find($id);
-    }
-
-    public function store($data)
-    {
-        $newItem = $this->model->create($data);
-
-        return $newItem;
-    }
-
-    public function delete($id)
-    {
-        return $this->getItem($id)->delete();
     }
 }
