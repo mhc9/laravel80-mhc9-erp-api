a34074b45096084e0e7dd23b590c30f954e2e3ef
sanyath007 <sanyath007@gmail.com>
2025-04-17 11:34:17 +0700

    fix(LoanRefundController): fix error in getForm method

--- app/Http/Controllers/LoanRefundController.php
+++ app/Http/Controllers/LoanRefundController.php
@@ -551,7 +551,7 @@ class LoanRefundController extends Controller
         if ($refund->refund_type_id == 1) {
             $word->setValue('refundType', 'และคืนเงินยืม จำนวนเงิน ' . number_format($refund->balance, 2) . ' บาท (' . baht_text($refund->balance, 2) . ')');
         } else if ($refund->refund_type_id == 2) {
-            $word->setValue('refundType', 'และเบิกเงินเพิ่ม จำนวนเงิน ' . number_format(abs($refund->balance, 2)) . ' บาท (' . baht_text(abs($refund->balance, 2)) . ')');
+            $word->setValue('refundType', 'และเบิกเงินเพิ่ม จำนวนเงิน ' . number_format(abs($refund->balance)) . ' บาท (' . baht_text(abs($refund->balance)) . ')');
         } else {
             $word->setValue('refundType', '');
         }
