e7e967ceaa9c6a947db137abb75b33c411f9aba9
sanyath007 <sanyath007@gmail.com>
2025-04-21 16:44:01 +0700

    reflactor(TaskController): remove unused methods and imported libreaies

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,9 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Validation\Rule;
-use Illuminate\Support\MessageBag;
-use Phattarachai\LineNotify\Facade\Line;
 use App\Models\Task;
 use App\Models\TaskType;
 use App\Models\TaskGroup;
@@ -18,50 +15,6 @@ use App\Models\Employee;
 
 class TaskController extends Controller
 {
-    public function formValidate (Request $request)
-    {
-        $rules = [
-            'plan_type_id'      => 'required',
-            'item_name'         => 'required',
-            'unit_id'           => 'required',
-        ];
-
-        if ($request['is_addon'] != '1') {
-            $rules['category_id'] = 'required';
-            $rules['price_per_unit'] = 'required';
-        }
-
-        $messages = [
-            'plan_type_id.required'     => 'กรุณาเลือกประเภทแผน',
-            'category_id.required'        => 'กรุณาเลือกประเภทสินค้า/บริการ',
-            'item_name.required'        => 'กรุณาระบุชื่อสินค้า/บริการ',
-            'price_per_unit.required'   => 'กรุณาระบุราคาต่อหน่วย',
-            'unit_id.required'          => 'กรุณาเลือกหน่วยนับ',
-        ];
-
-        $validator = \Validator::make($request->all(), $rules, $messages);
-
-        if ($validator->fails()) {
-            $messageBag = $validator->getMessageBag();
-
-            // if (!$messageBag->has('start_date')) {
-            //     if ($this->isDateExistsValidation(convThDateToDbDate($request['start_date']), 'start_date') > 0) {
-            //         $messageBag->add('start_date', 'คุณมีการลาในวันที่ระบุแล้ว');
-            //     }
-            // }
-
-            return [
-                'success' => 0,
-                'errors' => $messageBag->toArray(),
-            ];
-        } else {
-            return [
-                'success' => 1,
-                'errors' => $validator->getMessageBag()->toArray(),
-            ];
-        }
-    }
-
     private function getEmployeeList($fname, $lname)
     {
         return Employee::where('firstname', 'like', '%'.$fname.'%')
